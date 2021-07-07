6387b7c1712f8da95fb41396495786c99a49d210
mokuzaru <kenny.otoya@pucp.pe>
2021-07-07 06:15:31 -0500

    put ino

--- ard_dri/ard_dri.ino
+++ ard_dri/ard_dri.ino
@@ -4,13 +4,24 @@
  * ard- rpi
  * 
  * ard
+ * ---
  * pin2 left
  * pin3 right 
  * 
  * rpi
+ * ---
  * gpio14 left
  * gpio15 right
  * 
+ * sensor 1
+ * --------
+ * trig 7
+ * eco 8
+ * 
+ * Sensor 2
+ * -------
+ * trig2 11
+ * eco2 12
  */
 
 const int interpin1 = 2;
@@ -23,6 +34,8 @@ const int dirD = 6;
 
 int trig = 7;
 int eco = 8;
+int trig2 = 11;
+int eco2 = 12;
 
 long duration;
 int distance;
@@ -34,11 +47,13 @@ int y = 80;
 void setup() {
   Serial.begin(9600);
   pinMode(13,OUTPUT);
-  pinMode(interrpin1, INPUT_PULLUP);
-  pinMode(interrpin2, INPUT_PULLUP);
+  pinMode(interpin1, INPUT_PULLUP);
+  pinMode(interpin2, INPUT_PULLUP);
 
   pinMode(trig,OUTPUT);
   pinMode(eco, INPUT);
+  pinMode(trig2,OUTPUT);
+  pinMode(eco2,INPUT);
 
   pinMode(motI, OUTPUT);
   pinMode(motD, OUTPUT);
@@ -49,21 +64,67 @@ void setup() {
   digitalWrite(motD, LOW);
   digitalWrite(dirI, HIGH);
   digitalWrite(dirD, LOW);
-
+/*
   left();
   delay(1500);
   der();
   delay(1500);
+  */
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   while(digitalRead(2) == 0){
     left();
   }
   while(digitalRead(3) == 0){
     der();
   }
+  dist(trig, eco);
+  
+  if(distance < 50 || distance > 150){
+    para();
+  }else if(digitalRead(2) == 1 || digitalRead(3)== 1){
+    if(distance >120 && distance < 150){
+      adelante();
+    }
+    if(distance < 80 && distance > 50){
+      dist(trig2,eco2);
+      Serial.print("distancia atras: ");
+      Serial.println(distance);
+      if(distance > 40){
+        atras();
+        distance = 0;   
+      }      
+    }
+  }
+  delay(500);
+}
+
+void adelante(){
+  Serial.println("adelante");
+  digitalWrite(dirD, LOW);
+  digitalWrite(dirI, HIGH);
+  delay(50);
+  analogWrite(motI, x);
+  analogWrite(motD, x);
+  delay(700);
+  analogWrite(motI, 0);
+  analogWrite(motD, 0);
+  delay(50);
+}
+
+void atras() {
+  Serial.println("atras");
+  digitalWrite(dirD, HIGH);
+  digitalWrite(dirI, LOW);
+  delay(50);
+  analogWrite(motI, x);
+  analogWrite(motD, x);
+  delay(700);
+  analogWrite(motI, 0);
+  analogWrite(motD, 0);
+  delay(50);
 }
 
 void left() {
@@ -107,8 +168,6 @@ void para() {
 }
 
 void dist (int a, int b) {
-  Serial.println("start medida");
-
   digitalWrite(a, HIGH);
   delayMicroseconds(2);
   digitalWrite(a, LOW);
