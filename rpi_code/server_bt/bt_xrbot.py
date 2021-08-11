4a79295c4cdc9c9a3c9ef713a8dd3ff13a180288
mokuzaru <kenny.otoya@pucp.pe>
2021-08-11 12:36:50 -0500

    fix server bt

---
+++ rpi_code/server_bt/bt_xrbot.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+
+import dbus
+
+from advertisement import Advertisement
+from service import Application, Service, Characteristic, Descriptor
+from gpiozero import CPUTemperature
+from gpiozero import LED
+from time import sleep
+led1 = LED(18)
+led2 = LED(17)
+GATT_CHRC_IFACE = "org.bluez.GattCharacteristic1"
+NOTIFY_TIMEOUT = 5000
+
+class ThermometerAdvertisement(Advertisement):
+    def __init__(self, index):
+        Advertisement.__init__(self, index, "peripheral")
+        self.add_local_name("XRobot")
+        self.include_tx_power = True
+
+class ThermometerService(Service):
+    THERMOMETER_SVC_UUID = "00000001-710e-4a5b-8d75-3e5b444bc3cf"
+
+    def __init__(self, index):
+        self.farenheit = True
+
+        Service.__init__(self, index, self.THERMOMETER_SVC_UUID, True)
+        self.add_characteristic(TempCharacteristic(self))
+        self.add_characteristic(UnitCharacteristic(self))
+
+    def is_farenheit(self):
+        return self.farenheit
+
+    def set_farenheit(self, farenheit):
+        self.farenheit = farenheit
+
+class TempCharacteristic(Characteristic):
+    TEMP_CHARACTERISTIC_UUID = "00000002-710e-4a5b-8d75-3e5b444bc3cf"
+
+    def __init__(self, service):
+        self.notifying = False
+
+        Characteristic.__init__(
+                self, self.TEMP_CHARACTERISTIC_UUID,
+                ["notify", "read"], service)
+        self.add_descriptor(TempDescriptor(self))
+
+    def get_temperature(self):
+        value = []
+        unit = "C"
+
+        cpu = CPUTemperature()
+        temp = cpu.temperature
+
+        strtemp = str(round(temp, 1)) + " " + unit
+        for c in strtemp:
+            value.append(dbus.Byte(c.encode()))
+
+        return value
+
+    def set_temperature_callback(self):
+        if self.notifying:
+            value = self.get_temperature()
+            self.PropertiesChanged(GATT_CHRC_IFACE, {"Value": value}, [])
+
+        return self.notifying
+
+    def StartNotify(self):
+        if self.notifying:
+            return
+
+        self.notifying = True
+
+        value = self.get_temperature()
+        self.PropertiesChanged(GATT_CHRC_IFACE, {"Value": value}, [])
+        self.add_timeout(NOTIFY_TIMEOUT, self.set_temperature_callback)
+
+    def StopNotify(self):
+        self.notifying = False
+
+    def ReadValue(self, options):
+        value = self.get_temperature()
+
+        return value
+
+class TempDescriptor(Descriptor):
+    TEMP_DESCRIPTOR_UUID = "2901"
+    TEMP_DESCRIPTOR_VALUE = "CPU Temperature"
+
+    def __init__(self, characteristic):
+        Descriptor.__init__(
+                self, self.TEMP_DESCRIPTOR_UUID,
+                ["read"],
+                characteristic)
+
+    def ReadValue(self, options):
+        value = []
+        desc = self.TEMP_DESCRIPTOR_VALUE
+
+        for c in desc:
+            value.append(dbus.Byte(c.encode()))
+
+        return value
+
+class UnitCharacteristic(Characteristic):
+    UNIT_CHARACTERISTIC_UUID = "00000003-710e-4a5b-8d75-3e5b444bc3cf"
+
+    def __init__(self, service):
+        Characteristic.__init__(
+                self, self.UNIT_CHARACTERISTIC_UUID,
+                ["read", "write"], service)
+        self.add_descriptor(UnitDescriptor(self))
+#important function (reciving data)
+    def WriteValue(self, value, options):
+        val = str(value[0]).upper()
+        print(val)
+        if val == "C":
+            #Prepare to 3d modeling
+            print("Se prepara para modelo 3d")
+        elif val == "F":
+            #Inicia la video llamada
+            print("Inicia video llamada")
+            led3.on()
+        elif val == "A":
+            #Start model 3d
+            print("Inicia modelo 3d")
+            led4.on()
+        elif val == "B":
+            #End model 3d
+            print("Termina modelo 3d")
+            led4.off()
+        elif val == "M":
+            #End videocall
+            led3.off()
+            print("termina llamada")
+
+    def ReadValue(self, options):
+        value = []
+
+        value.append(dbus.Byte(val.encode()))
+
+        return value
+
+class UnitDescriptor(Descriptor):
+    UNIT_DESCRIPTOR_UUID = "2901"
+    UNIT_DESCRIPTOR_VALUE = "C para girar a la izquierda"
+
+    def __init__(self, characteristic):
+        Descriptor.__init__(
+                self, self.UNIT_DESCRIPTOR_UUID,
+                ["read"],
+                characteristic)
+
+    def ReadValue(self, options):
+        value = []
+        desc = self.UNIT_DESCRIPTOR_VALUE
+
+        for c in desc:
+            value.append(dbus.Byte(c.encode()))
+
+        return value
+
+app = Application()
+app.add_service(ThermometerService(0))
+app.register()
+
+adv = ThermometerAdvertisement(0)
+adv.register()
+
+try:
+    app.run()
+except KeyboardInterrupt:
+    app.quit()
